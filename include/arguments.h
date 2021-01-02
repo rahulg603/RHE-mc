cd022055fe73e583c77f1eb9295c340cf19d351f
Rahul <rahulg603@gmail.com>
2021-01-01 19:49:07 -0500

    Resolve is_same compile error by commenting these definitions out, see issue 1 in the repository for RHE-reg https://github.com/sriramlab/RHE-reg/issues/1

--- include/arguments.h
+++ include/arguments.h
@@ -34,15 +34,15 @@ struct options{
 	double beta;
 };
 
-template<typename T, typename U>
-struct is_same {
-    static const bool value = false; 
-};
+//template<typename T, typename U>
+//struct is_same {
+//    static const bool value = false; 
+//};
 
-template<typename T>
-struct is_same<T,T> { 
-   static const bool value = true; 
-};
+//template<typename T>
+//struct is_same<T,T> { 
+//   static const bool value = true; 
+//};
 
 
 extern options command_line_opts;
